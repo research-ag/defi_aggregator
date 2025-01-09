a1bab33c8a4a3172c3505800d07f7ca27c3bcbca
Timo Hanke <t@timohanke.org>
2025-01-09 22:31:39 +0100

    Move mops.sources from build.sh to Dockerfile

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+MOC_ARGS=$(cat mops.sources)
 OUT=out/out_$(uname -s)_$(uname -m).wasm
-moc `mops sources` src/main.mo --public-metadata candid:service --public-metadata candid:args -o $OUT
+moc src/main.mo --public-metadata candid:service --public-metadata candid:args -o $OUT ${MOC_ARGS}
 sha256sum $OUT
